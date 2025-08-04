a4af0c8f5bdbf8abc671b6c9fb61cd2b2f1f2b9a
kenneth <kenneth12345645@gmail.com>
2025-08-04 14:09:56 +0800

    Add in dispatcher header file

---
+++ akka/Dispatcher.hpp
@@ -0,0 +1,23 @@
+#include <functional>
+#include <vector>
+#include <thread>
+#include <queue>
+
+class Dispatcher : std::enable_shared_from_this<Dispatcher>
+{
+    std::vector<std::thread> workers_;
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    bool stop_ = false;
+    std::queue<std::function<void()>> tasks_;
+
+public:
+    explicit Dispatcher(int numThreads = 4);
+    void submit(std::function<void()>);
+    ~Dispatcher();
+};
+
+// std::enable_shared_from_this is a C++ utility that allows an object, which is already managed by a std::shared_ptr, to safely obtain a new std::shared_ptr to itself
+// from within its own member functions.
+
+// This is the dispatcher, which contains tasks, which are std::function<void()>, std::vector<std::thread> workers, a mutex, a condition variable,
