91e3d285348ef20a7b1e47ef24c2364b6c8c87ae
kenneth <kenneth12345645@gmail.com>
2025-08-04 21:03:15 +0800

    Added actorRef header file

---
+++ akka/ActorRef.hpp
@@ -0,0 +1,12 @@
+#include "ActorInstance.hpp"
+
+class ActorRef
+{
+    std::shared_ptr<ActorInstance> actorInstance_;
+
+public:
+    explicit ActorRef(std::shared_ptr<ActorInstance> actorInstance);
+    void tell(std::string &message);
+};
+
+// ActorRef is a reference to an actorInstance. This provides an interface for us to create reference to actor instances, and to pass messages to it.
