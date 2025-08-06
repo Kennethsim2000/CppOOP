ec18e43c8ab13bc1d53121a1ee530281e90fa37c
kenneth <kenneth12345645@gmail.com>
2025-08-06 09:25:36 +0800

    Complete actor system implementation

--- akka/ActorSystem.cpp
+++ akka/ActorSystem.cpp
@@ -3,10 +3,15 @@
 
 ActorSystem::ActorSystem()
 {
+    dispatcher_ = std::make_shared<Dispatcher>(4);
 }
 
 ActorRef ActorSystem::spawn(std::shared_ptr<Actor> actor)
 {
+    ActorRef actorRef = ActorRef(std::make_shared<ActorInstance>(dispatcher_, std::move(actor)));
+    return actorRef;
 }
 
-// TODO: Complete implementation of ActorSystem
+// Why do we move the actor and not the dispatcher_? Because dispatcher_ is a member variable of ActorSystem, and you likely want to keep it alive as long as the system lives. If we move,
+// the reference count of the shared pointer stays the same, meaning that this dispatcher may be freed once the ActorRef terminates.
+// You should only std::move a member field if you no longer need it afterward. In this case, an actor system can be used to spawn multiple actors, so we should pass copies of the dispatcher.
