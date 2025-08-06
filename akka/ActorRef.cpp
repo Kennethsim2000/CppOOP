0f9490a2f07d9f2254666519ad6479e88340de01
kenneth <kenneth12345645@gmail.com>
2025-08-06 09:13:14 +0800

    Complete implementation of actorRef

--- akka/ActorRef.cpp
+++ akka/ActorRef.cpp
@@ -1,11 +1,10 @@
 #include "ActorRef.hpp"
 
-ActorRef::ActorRef(std::shared_ptr<ActorInstance> actorInstance) : actorInstance_(actorInstance)
+ActorRef::ActorRef(std::shared_ptr<ActorInstance> actorInstance) : actorInstance_(std::move(actorInstance))
 {
 }
 
 void ActorRef::tell(std::string &message)
 {
+    actorInstance_->enqueue(message);
 }
-
-// TODO: Complete ActorRef.cpp implementation
