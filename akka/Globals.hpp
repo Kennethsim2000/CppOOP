411093cae271c04b7042c83b15c404aecbd5713f
kenneth <kenneth12345645@gmail.com>
2025-08-11 22:23:43 +0800

    Add in explanation comments

--- akka/Globals.hpp
+++ akka/Globals.hpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <mutex>
 
-namespace globals
+namespace globals // namespace are used to group related variables/functions together to avoid collision
 {
-    extern std::mutex cout_mutex;
+    extern std::mutex cout_mutex; // extern means that this is a declaration, not a definition.
+    // this line does not create the mutex, it just says that in a .cpp file, there will be std::mutex cout_mutex
 }
